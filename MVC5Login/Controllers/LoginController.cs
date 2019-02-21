ebf74a446dfcdacbb10024e7f8d55c0dbd556fe6
shaileshs <shaileshs@SHAILESH>
2019-02-21 16:52:08 +0530

    Add project files.

---
+++ MVC5Login/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using MVC5Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(User userModel)
+        {
+            using (LoginEntities db = new LoginEntities())
+            {
+                var userDetails = db.Users.Where(x => x.Username == userModel.Username && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.Username;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
